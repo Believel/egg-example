164880ca1e379047b9e389f195156e24108f49d8
panp.zhang001 <panp.zhang001@bkjk.com>
2021-03-22 16:19:14 +0800

    init

---
+++ app/controller/news.js
@@ -0,0 +1,22 @@
+// app/controller/news.js
+
+const Controller = require('egg').Controller;
+class NewsController extends Controller {
+    async list() {
+        // !读取写死的数据
+        // const dataList = {
+        //     list: [
+        //         { id: 1, title: 'this is news 1', url: '/news/1'},
+        //         { id: 2, title: 'this is news 2', url: '/news/2'}
+        //     ]
+        // };
+        // await this.ctx.render('news/list.hbs', dataList);
+
+        // !从服务器中获取数据
+        const ctx = this.ctx;
+        const page = ctx.query.page || 1;
+        const newsList = await ctx.service.news.list(page);
+        await ctx.render('news/list.hbs', { list: newsList});
+    }
+}
+module.exports = NewsController
